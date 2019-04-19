04b8bc96821175f86aed594d4866c59c17c5b3a0
geff <primaver@seattleu.edu>
2019-04-18 17:48:49 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -1,12 +1,11 @@
 {
   "data": [
     {
-      "name": "James Bond",
-      "designation": "Accountant",
-      "salary": "$170,750",
-      "joining_date": "2011/07/25",
-      "office": "Tokyo",
-      "extension": "8422"
+      "name": "Teresita Spring",
+      "relationship": "Mother",
+      "rsvp": "Yes",
+      "departure location": "Seattle, WA",
+      "dinner preference": "Meat",
     }
   ]
 }
