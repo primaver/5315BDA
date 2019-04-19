a30f587a2eab4ccf20918a34662309de215b245d
geff <primaver@seattleu.edu>
2019-04-18 18:00:15 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -4,7 +4,7 @@
       "name": "Teresita Spring",
       "relationship": "Mother",
       "rsvp": "Yes",
-      "departure_location": "Seattle, WA",
+      "departure_location": "Seattle,WA",
       "dinner_preference": "Meat",
     }
   ]
