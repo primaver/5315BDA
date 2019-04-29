5fbf7f920234dc5ed9dceb0ee5aeec9a37c1eb79
geff <primaver@seattleu.edu>
2019-04-28 19:57:52 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -2,12 +2,11 @@
   "data": [
     {
       "name": "James Bond",
-      "hourly_rate": "$10.00",
       "job_title": "Office Administrator",
-      "shift": "1",
       "start_date": "01/01/2019",
-
-
+      "shift": "1",
+      "hourly_rate": "$10.00"
+      
     }
   ]
 }
