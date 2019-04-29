7ac522445b0085789d5ced1c3fde34a9a0f42391
geff <primaver@seattleu.edu>
2019-04-29 15:51:32 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -5,7 +5,13 @@
       "job_title": "Office Administrator",
       "start_date": "01/01/2019",
       "shift": "1",
-      "hourly_rate": "$10.00"
+      "hourly_rate": "$10.00",
+
+      "name": "Sean Connery",
+      "job_title": "Janitor",
+      "start_date": "02/01/2019",
+      "shift": "2",
+      "hourly_rate": "$15.00"
       
     }
   ]
