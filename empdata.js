716da7440ffda7deb2069c43ca33143e26c0b97f
geff <primaver@seattleu.edu>
2019-04-28 19:35:39 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -2,7 +2,7 @@
   "data": [
     {
       "name": "James Bond",
-      "designation": "Accountant",
+      "hourly_rate": "$10.00",
       "salary": "$170,750",
       "joining_date": "2011/07/25",
       "office": "Tokyo",
