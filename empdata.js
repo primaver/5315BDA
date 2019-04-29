f2d583f9e3b201ea3002f472549b73eee45d82b8
geff <primaver@seattleu.edu>
2019-04-29 15:59:19 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -15,6 +15,14 @@
       "shift": "2",
       "hourly_rate": "$15.00"
       
+    },
+    { 
+      "name": "Pierce Bronson",
+      "job_title": "Operations Manager",
+      "start_date": "03/01/2019",
+      "shift": "3",
+      "hourly_rate": "$20.00"
+      
     }
   ]
 }
