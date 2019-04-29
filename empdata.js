973e81ad5d876e2588470eeb00b657ff74a5adf9
geff <primaver@seattleu.edu>
2019-04-28 17:48:38 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -2,11 +2,11 @@
   "data": [
     {
       "name": "Quinn Flynn",
-      "designation": "Yes",
+      "reservation": "Yes",
       "salary": "$342,000",
       "joining_date": "2013/03/03",
       "office": "Edinburgh",
       "extension": "9497"
-    },
+    }
   ]
 }
